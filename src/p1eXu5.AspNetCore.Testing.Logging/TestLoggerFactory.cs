654a945eb8ce066227b0ecf6f178c404c4afcb01
p1eXu5 <v1adp1exu5@gmail.com>
2024-02-23 17:52:40 +0300

    1.0.12: add TestLoggerFactory

---
+++ src/p1eXu5.AspNetCore.Testing.Logging/TestLoggerFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace p1eXu5.AspNetCore.Testing.Logging;
+
+public class TestLoggerFactory : ILoggerFactory
+{
+    private readonly ITestContextWriters _testContextWriters;
+    private readonly LogOut _logOut;
+
+    public TestLoggerFactory(ITestContextWriters testContextWriters, LogOut logOut = LogOut.All)
+    {
+        _testContextWriters = testContextWriters;
+        _logOut = logOut;
+    }
+
+    public static ILoggerFactory CreateWith(TextWriter? progressWriter, TextWriter? outWriter)
+        => new TestLoggerFactory(TestContextWriters.DefaultWith(progressWriter, outWriter));
+
+    public void AddProvider(ILoggerProvider provider)
+    {
+    }
+
+    public ILogger CreateLogger(string categoryName)
+        => new TestLogger(_testContextWriters, categoryName, _logOut);
+
+    public void Dispose()
+    {
+    }
+}
